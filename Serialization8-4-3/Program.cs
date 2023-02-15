09b99eff13cd3b014bd9dd0650e200b336349a2b
Alexey <visotskiy.alexey@gmail.com>
2023-02-15 22:32:23 +0700

    Сериализация - десериализация

---
+++ Serialization8-4-3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization8_4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var cnt = new Contact("Petya", 777, "petya@mail.ru");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (var stream = new FileStream("contact.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(stream, cnt);
+            }
+
+            using (var fs = new FileStream("contact.dat", FileMode.OpenOrCreate))
+            {
+                var contact = (Contact)formatter.Deserialize(fs);
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($" {contact.Name} --- {contact.PhoneNumber}");
+            }
+
+        }
+    }
+
+    [Serializable]
+    class Contact
+    {
+        public string Name { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string name, long phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+    
+}
