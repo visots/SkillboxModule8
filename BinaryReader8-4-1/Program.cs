f37716b5ff4d3eb39ec72768860fcd54dc58289a
Alexey <visotskiy.alexey@gmail.com>
2023-02-15 22:08:27 +0700

    Binary reader\Writer 8-4-1

---
+++ BinaryReader8-4-1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace BinaryReader8_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\visot\source\repos\SkillFactory\SkillboxModule8\BinaryReader8-4-1\BinaryFile.bin";
+            
+            //WriteBinFile(filePath);
+            ReadBinFile(filePath);
+
+
+        }
+
+        static void ReadBinFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            var stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                Console.WriteLine(reader.ReadString());
+            }
+            stream.Close();
+        }
+
+        static void WriteBinFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            var stream = File.Open(filePath, FileMode.Open, FileAccess.Write);
+
+            using (BinaryWriter bw = new BinaryWriter(stream))
+            {
+                bw.Write($"Файл был создан {DateTime.Now} на компьютере в Win10");
+            }
+
+            stream.Close();
+        }
+
+       
+    }
+}
--- File8.3/Program.cs
+++ File8.3/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace File8._3
+namespace File8._3
 {
     internal class Program
     {
@@ -6,9 +6,16 @@
         {
             string filePath = @"C:\Users\visot\source\repos\SkillFactory\SkillboxModule8\File8.3\Program.cs";
 
-            if(File.Exists(filePath))
+            var file = new FileInfo(filePath);
+
+            if(file.Exists)
             {
-                using (StreamReader sr = File.OpenText(filePath))
+                using (StreamWriter sw = file.AppendText())
+                {
+                    sw.WriteLine("//" + DateTime.Now);
+                }
+
+                using (StreamReader sr = file.OpenText())
                 {
                     string data = "";
                     while((data = sr.ReadLine()) != null)
@@ -19,4 +26,4 @@
             }
         }
     }
-}
+}//15.02.2023 21:28:34
