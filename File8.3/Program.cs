1e2c8ec91ef46918c425f41a8abc9f1e38af40ba
Alexey <visotskiy.alexey@gmail.com>
2023-02-15 21:00:27 +0700

    8-3-1 Вывод текста программы

---
+++ File8.3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace File8._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\visot\source\repos\SkillFactory\SkillboxModule8\File8.3\Program.cs";
+
+            if(File.Exists(filePath))
+            {
+                using (StreamReader sr = File.OpenText(filePath))
+                {
+                    string data = "";
+                    while((data = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(data);
+                    }
+                }
+            }
+        }
+    }
+}
