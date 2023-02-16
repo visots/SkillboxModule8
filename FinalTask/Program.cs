a72b66e5ef19831e738f848a617b7e2d37b5951b
Alexey <visotskiy.alexey@gmail.com>
2023-02-17 00:12:03 +0700

    Итоговое задание 8.6.4

---
+++ FinalTask/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Путь к исходному файлу с данными
+            string inputDataPath = args[0];
+
+            if (String.IsNullOrEmpty(inputDataPath))
+            {
+                Console.WriteLine("Путь не может быть пустым");
+                Console.ReadKey();
+                Environment.Exit(Environment.ExitCode);
+            }
+
+            if (!File.Exists(inputDataPath))
+            {
+                Console.WriteLine("Не найден файл " + inputDataPath);
+                Console.ReadKey();
+                Environment.Exit(Environment.ExitCode);
+            }
+
+            try
+            {
+                Console.WriteLine("Получение данных");
+                var students = ReadData(inputDataPath);
+                var filesPath = CreateDirectory();
+                Console.WriteLine("Данные получены\nСоздаем файлы данных");
+                CreateDataFiles(students, filesPath);
+                Console.WriteLine("Файлы сохранены в директорию "+filesPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка!\n" + ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+        static Student[] ReadData(string filePath)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            Student[] students;
+
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                students = (Student[])formatter.Deserialize(stream);
+            }
+
+            return students;
+        }
+
+        static string CreateDirectory()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            DirectoryInfo dir = new DirectoryInfo(path + @"\Students");
+            if (!dir.Exists)
+            {
+                dir.Create();
+            }
+            return dir.FullName;
+        }
+
+        static void CreateDataFiles(Student[] students, string path)
+        {
+            foreach (Student student in students.OrderBy(s => s.Group))
+            {
+                string filePath = $"{path}\\{student.Group}.txt";
+                string text = student.Name + ", " + student.DateOfBirth.ToString("dd-MM-yyyy");
+                
+                WriteToFile(text, filePath);
+            }
+        }
+
+        static void WriteToFile(string inputText, string filePath)
+        {
+            using (StreamWriter sw = File.AppendText(filePath))
+            {
+                sw.WriteLine(inputText);
+            }
+        }
+    }
+}
---
+++ FinalTask/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask
+{
+    [Serializable]
+    internal class Student
+    {
+        public string Name { get; set; }
+        
+        public string Group { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
